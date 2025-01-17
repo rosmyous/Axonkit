785242d07f0d7042e1dc08c8591226635fe5b4ef
AxonKit <myoussief111@mail.com>
2025-01-17 12:52:36 +0100

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    minimumCacheTTL: 60,
+  },
+  experimental: {
+    optimizeCss: true,
+    optimizePackageImports: ["framer-motion"],
+  },
+};
+
+export default nextConfig;
